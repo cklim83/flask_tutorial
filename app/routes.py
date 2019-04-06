5a28ada6fff0c0cd59954c5be371b5f0aab0c789
cklim <cheekiang.lim@gmail.com>
2019-04-06 15:41:28 +0800

    chapter_2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import my_app
+from flask import render_template
 
 @my_app.route('/')
 @my_app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Caleb'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template("index.html", title="Home", user=user, posts=posts)
