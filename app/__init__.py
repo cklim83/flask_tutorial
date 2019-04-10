81c4a7af2817995ae15a1e5493f151d5162128b9
cklim <cheekiang.lim@gmail.com>
2019-04-10 20:37:03 +0800

    Completed Chapter 4 Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 my_app = Flask(__name__)
 my_app.config.from_object(Config)
+db = SQLAlchemy(my_app)
+migrate = Migrate(my_app, db)
 
-from app import routes
+from app import routes, models
